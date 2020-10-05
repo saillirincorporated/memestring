df46d63eae8a538c2f3566662dc6bff5631968c4
Tanguy Spits <tspits@student.uliege.be>
2020-10-06 00:30:14 +0200

    New feature

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ docker run -it --rm <image_name> [args...]
 ```
 
 Prints the memestring from the args, try it out with the default args of the image
+
+
+At the request of someone else, if you supply the s flag to memestring then it will print in a retarded manner
+
+```sh
+docker build -t <image_name> .
+docker run -it --rm <image_name> [-s] [args...]
+```
+
--- main.go
+++ main.go
@@ -4,23 +4,46 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"flag"
+	"math/rand"
+	"unicode"
 )
 
-func main(){
-	args := os.Args[1:]
-	if len(args) < 1{
-		println("This program expects exactly one argument")
-		os.Exit(-1)
-	}
-	concatenated_args := strings.Join(args, " ")
-	concatenated_args = strings.ToUpper(concatenated_args)
-	fmt.Println(concatenated_args)
-	for i, v := range concatenated_args[1:]{
+func dumbmeme(s string){
+	fmt.Println(s)
+	for i, v := range s[1:]{
 		fmt.Printf("%c", v)
 		for j := 0; j < i; j++{
 			fmt.Printf("%c", ' ')
 		}
 		fmt.Printf("%c\n", v)
+	}
+}
+
+func stupidmeme(s string){
+	r := rand.New(rand.NewSource(99))
+	for _, v := range s{
+		if(r.Int() % 2 == 0){
+			fmt.Printf("%c", v)
+		}else{
+			fmt.Printf("%c", unicode.ToUpper(v))
+		}
+	}
+	fmt.Println("")
+}
+
+func main(){
+	boolFlag := flag.Bool("s", false, "If this is set then the args will be printed in a stupid manner")
+	flag.Parse()
+	if len(flag.Args()) < 1{
+		println("This program expects at least one non-flag argument")
+		os.Exit(-1)
+	}
+	concatenated_args := strings.Join(flag.Args(), " ")
+	if *boolFlag{
+		stupidmeme(concatenated_args)
+	}else{
+		dumbmeme(concatenated_args)
 	}
 	os.Exit(0)
 }
