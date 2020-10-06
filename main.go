a23a9a122c4c4e466cf48da458823f4a985f5184
Tanguy Spits <tspits@student.uliege.be>
2020-10-06 21:20:54 +0200

    Can run as server now

--- main.go
+++ main.go
@@ -8,36 +8,90 @@ import (
 	"math/rand"
 	"unicode"
 	"io/ioutil"
+	"net"
+	"strconv"
 )
 
-func dumbmeme(s string){
-	fmt.Println(s)
+func dumbmeme(s string) string{
+	var b strings.Builder
+	b.WriteString(strings.ReplaceAll(s, "\n", " "))
+	b.WriteString("\n")
 	for i, v := range s[1:]{
-		fmt.Printf("%c", v)
+		fmt.Fprintf(&b, "%c", v)
 		for j := 0; j < i; j++{
-			fmt.Printf("%c", ' ')
+			fmt.Fprintf(&b, "%c", ' ')
 		}
-		fmt.Printf("%c\n", v)
+		fmt.Fprintf(&b, "%c\n", v)
 	}
+	return b.String()
 }
 
-func stupidmeme(s string){
+func stupidmeme(s string) string{
 	r := rand.New(rand.NewSource(99))
+	var b strings.Builder
 	for _, v := range s{
 		if(r.Int() % 2 == 0){
-			fmt.Printf("%c", v)
+			fmt.Fprintf(&b, "%c", v)
 		}else{
-			fmt.Printf("%c", unicode.ToUpper(v))
+			fmt.Fprintf(&b, "%c", unicode.ToUpper(v))
 		}
 	}
-	fmt.Println("")
+	b.WriteString("\n")
+	return b.String()
+}
+
+func handleClientRequest(conn net.Conn, boolFlag *bool){
+	buf := make([]byte, 1024)
+	var err error = nil
+	var n int
+	for err == nil{
+		n, err = conn.Read(buf)
+		if err != nil{
+			continue
+		}
+		s := string(buf[:n])
+		if *boolFlag{
+			conn.Write([]byte(stupidmeme(s)))
+		}else{
+			conn.Write([]byte(dumbmeme(s)))
+		}
+	}
+	conn.Close()
+}
+
+func server(boolFlag *bool, portFlag *int, hostFlag *string){
+	var l net.Listener
+	var err error
+	fmt.Println(*hostFlag + ":" + strconv.Itoa(*portFlag));
+	if l, err = net.Listen("tcp", *hostFlag + ":" + strconv.Itoa(*portFlag)); err != nil{
+		println("Could not listen")
+		println(err.Error())
+		os.Exit(-1)
+	}
+	defer l.Close()
+	for {
+		var conn net.Conn
+		if conn, err = l.Accept(); err != nil{
+			println("Error accepting a connection")
+			continue
+		}
+		go handleClientRequest(conn, boolFlag)
+
+	}
 }
 
 func main(){
 	mdFlag := flag.Bool("d", false, "If this is set then the output will be enclosed in ``` for markdown")
 	boolFlag := flag.Bool("s", false, "If this is set then the args will be printed in a stupid manner")
 	inputFlag := flag.String("i", "" , "Set what the input is (<filename> or - for stdin), by default it's the non-flag arguments")
+	listenFlag := flag.Bool("l", false, "Set this as a server listening on port 1234 of localhost by default")
+	portFlag := flag.Int("p", 1234, "When used in conjunction with -l, use to specify port, defaults to 1234")
+	hostFlag := flag.String("h", "localhost", "When used in conjuction with -l, specifies the IP address to listen to, defaults to localhost")
 	flag.Parse()
+
+	if *listenFlag{
+		server(boolFlag, portFlag, hostFlag)
+	}
 	var s string
 	if *inputFlag != ""{
 		var bytes []byte
@@ -68,9 +122,9 @@ func main(){
 	}
 
 	if *boolFlag{
-		stupidmeme(s)
+		fmt.Print(stupidmeme(s))
 	}else{
-		dumbmeme(s)
+		fmt.Print(dumbmeme(s))
 	}
 
 	if *mdFlag{
