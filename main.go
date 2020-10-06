3231e3a50af0fa0c00afbf46c7dc2dbcc5141b9c
Tanguy Spits <tspits@student.uliege.be>
2020-10-06 20:07:03 +0200

    Read from file or stdin

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"flag"
 	"math/rand"
 	"unicode"
+	"io/ioutil"
 )
 
 func dumbmeme(s string){
@@ -35,20 +36,41 @@ func stupidmeme(s string){
 func main(){
 	mdFlag := flag.Bool("d", false, "If this is set then the output will be enclosed in ``` for markdown")
 	boolFlag := flag.Bool("s", false, "If this is set then the args will be printed in a stupid manner")
+	inputFlag := flag.String("i", "args", "Set what the input is (<filename> or - for stdin), by default it's the non-flag arguments")
 	flag.Parse()
-	if len(flag.Args()) < 1{
-		println("This program expects at least one non-flag argument")
+	var s string
+	if *inputFlag != "args"{
+		var bytes []byte
+		var err error
+		if *inputFlag == "-"{
+			bytes, err = ioutil.ReadAll(os.Stdin)
+			if err != nil{
+				println("Error")
+				os.Exit(-1)
+			}
+		}else{
+			bytes, err = ioutil.ReadFile(*inputFlag)
+			if err != nil{
+				println("Error")
+				os.Exit(-1)
+			}
+		}
+		s = string(bytes)
+	}else if len(flag.Args()) < 1{
+		println("This program expects at least one non-flag argument with default input")
 		os.Exit(-1)
+	}else{
+		s = strings.Join(flag.Args(), " ")
 	}
-	concatenated_args := strings.Join(flag.Args(), " ")
+
 	if *mdFlag{
 		fmt.Println("```")
 	}
 
 	if *boolFlag{
-		stupidmeme(concatenated_args)
+		stupidmeme(s)
 	}else{
-		dumbmeme(concatenated_args)
+		dumbmeme(s)
 	}
 
 	if *mdFlag{
